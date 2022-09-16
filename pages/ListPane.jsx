51f28c7568da9aafac8f4dd958a706cccf7cde69
Vishal Venkatraman <vishal.venkatraman97@gmail.com>
2022-09-16 13:50:00 +0800

    UI - Basic List Pane (II)

--- pages/ListPane.jsx
+++ pages/ListPane.jsx
@@ -1,29 +1,46 @@
 import React, { Fragment } from 'react'
-import { DataGrid } from '@mui/x-data-grid'
+import { useSelector, useDispatch } from "react-redux"
+import { UPDATE_SPELLS } from './../actions/data_actions';
+import { UPDATE_HIGHLIGHTED_SPELL } from './../actions/app_actions';
+
 import {
     Paper,
     Table,
     TableBody,
     TableCell,
-    TableContainer,
     TableHead,
     TableRow,
-    TableBar,
     Skeleton,
     Button
 } from '@mui/material'
- 
 import { useState, useEffect } from 'react'
 
+
 const ListPane = () => {
-
-    const [spell_list, set_spell_list] = useState([]);
+    // State components
+    const [selected_spell, set_selected_spell] = useState("");
+    const [selected_spell_url, set_selected_spell_url] = useState("");
     const [error, set_error] = useState("");
+    
+    // Obtain list of spells from redux state.
+    const spell_list = useSelector(state => state.data.spells);
+    const highlighted_spell = useSelector(state => state.app.highlighted_spell);
 
-    const Filled_Row = (data) => {
-        console.log(data)
+    // Bind useDispatch function to a variable
+    const dispatch = useDispatch();
+
+    // Component defining a filled data row.
+    const Filled_Row = (data, key) => {
         return (
-            <TableRow>
+            <TableRow
+                hover
+                onClick={() => {
+                    let extracted_url = data.data.url.toString()
+                    dispatch({ type: UPDATE_HIGHLIGHTED_SPELL, payload: extracted_url })
+
+
+                }}
+            >
                 <TableCell>
                     {
                         data.data.name.toString()
@@ -37,7 +54,7 @@ const ListPane = () => {
             </TableRow>
         )
     }
-
+    // Component defining a placeholder row.
     const Loading_Row = () => {
         return (
             <TableRow>
@@ -49,10 +66,28 @@ const ListPane = () => {
             </TableRow>
     
         )
-    }    
-    // Call the DND5E API to obtain list of spells.
-    const fetchSpells = () => {
+    }
+
+    // Interface function for re-using fetch_spells for both spell list and specific spells. 
+    const prepare_fetch_spell = () => {
         const base_url = "https://www.dnd5eapi.co";
+        let spell_url = "";
+
+        // if (selected_spell_url.length === 0) {
+            spell_url = base_url + "/api/spells";
+        // }
+
+        // else {
+            // spell_url = base_url + selected_spell_url
+        // }
+
+        return spell_url;
+    }
+
+    // Function for calling the DND5E API to obtain a list of spells or individual spell data.
+    const fetch_spells = () => {
+        const spell_url = prepare_fetch_spell();
+
         const params = {
             "method": "GET",
             "headers": {
@@ -60,51 +95,55 @@ const ListPane = () => {
             }
         }
     
-        let spell_url = base_url + "/api/spells"
-    
         fetch(spell_url, params)
-        .then(response => {
-            if (!response.ok) {
-                set_error(response.status);
-            }
-            return response.json(); 
-        })
+            
+            .then(response => {
+                if (!response.ok) {
+                    set_error(response.status);
+                }
+                return response.json(); 
+            })
     
-        .then(data => {
-            set_spell_list(data.results)
-            set_error("");
-        })
-        .catch((err) => {
-            set_error(err.message)
-            set_spell_list([])
-        })
+            .then(data => {
+                // if (selected_spell_url.length !== 0) {
+                    // console.log(data);
+                    // set_selected_spell(data);
+                // }
+
+                // else{
+                // set_spell_list(data.results);
+                dispatch({ type: UPDATE_SPELLS, payload: data.results })
+
+                // }
+                set_error("");
+            })
+
+            .catch((err) => {
+                set_error(err.message);
+                // set_spell_list([]);
+            })
     }
     
-
     useEffect(() => {
-        fetchSpells()
+        fetch_spells();
     }, []);
 
 
     return (
         <Fragment>
-            <Button
-                onClick={() => {
-                    console.log(spell_list)
-                }}
-            >
-                API Test
-            </Button>
+
             <Paper
                 sx={{
                     width: "40vw",
-                    height: "95vh"
+                    height: "95vh",
+                    overflow: "scroll"
                 }}
             >
+
                 <Table>
-                    <TableHead>
+                    {/* <TableHead>
                         
-                    </TableHead>
+                    </TableHead> */}
 
                     <TableBody>
 
@@ -117,17 +156,15 @@ const ListPane = () => {
                                             data={spell}
                                             key={spell.index}
                                         />
-                                    // console.log(spell.name)
                                     )
 
                                 })
                         }
 
                     </TableBody>
+
                 </Table>
                 
-
-
             </Paper>
 
 
